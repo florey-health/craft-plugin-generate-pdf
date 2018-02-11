4781d044fd1480a4fba2345bc7037bf8ad3edd96
Torben Sko <torben.sko@nextpracticehealth.com>
2018-02-11 18:44:33 +1100

    Generated out a PDF. Support subsitutions, background images and remote fonts

--- src/twigextensions/GeneratePdfTwigExtension.php
+++ src/twigextensions/GeneratePdfTwigExtension.php
@@ -4,62 +4,93 @@
  *
  * Generate a PDF from a template.
  *
- * @link      https://torbensko.com
+ * @link    https://torbensko.com
  * @copyright Copyright (c) 2018 Torben Sko
  */
 
 namespace spg\generatepdf\twigextensions;
 
 use spg\generatepdf\GeneratePdf;
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 use Craft;
 
 /**
- * @author    Torben Sko
+ * @author  Torben Sko
  * @package   GeneratePdf
- * @since     1.0.0
+ * @since   1.0.0
  */
 class GeneratePdfTwigExtension extends \Twig_Extension
 {
-    // Public Methods
-    // =========================================================================
+  // Public Methods
+  // =========================================================================
 
-    /**
-     * @inheritdoc
-     */
-    public function getName()
-    {
-        return 'GeneratePdf';
+  /**
+   * @inheritdoc
+   */
+  public function getName()
+  {
+    return 'GeneratePdf';
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function getFilters()
+  {
+    return [
+      new \Twig_SimpleFilter('generatePdf', [$this, 'generatePdfFunction']),
+    ];
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function getFunctions()
+  {
+    return [
+      new \Twig_SimpleFunction('someFunction', [$this, 'generatePdfFunction']),
+    ];
+  }
+
+  /**
+   * @param null $text
+   *
+   * @return string
+   */
+  public function generatePdfFunction($templateHtml, $data, $backgroundImage = '')
+  {
+    $backgroundImage = preg_replace("/[#?].*$/", "", $backgroundImage);
+    $subsitutions = explode("\n", $data);
+
+    foreach($subsitutions as $keyValue) {
+      if (preg_match("/=/", $keyValue)) {
+        $parts = explode("=", $keyValue, 2);
+        $templateHtml = preg_replace("/{{[ ]*".$parts[0]."[ ]*}}/", $parts[1], $templateHtml);
+      }
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function getFilters()
-    {
-        return [
-            new \Twig_SimpleFilter('generatePdf', [$this, 'generatePdfFunction']),
-        ];
-    }
+    // return $backgroundImage;
+    $html = "";
+    $html .= "<html><head><link href=\"https://fonts.googleapis.com/css?family=Montserrat\" rel=\"stylesheet\"><style>html,body { margin:0; font-family: 'Montserrat', sans-serif; font-weight: 900; }</style></head><body>";
+    $html .= "<div style=\"position:absolute; left:0; right:0; width:100%; height:100%; background-size:cover; background-position:50%; background-image:url('{$backgroundImage}');\">{$templateHtml}</div>";
+    $html .= "</body></html>";
 
-    /**
-     * @inheritdoc
-     */
-    public function getFunctions()
-    {
-        return [
-            new \Twig_SimpleFunction('someFunction', [$this, 'generatePdfFunction']),
-        ];
-    }
+    // instantiate and use the dompdf class
+    $options = new Options();
+    $options->set('isRemoteEnabled', true);
+    // $options->set('defaultFont', 'Courier');
 
-    /**
-     * @param null $text
-     *
-     * @return string
-     */
-    public function generatePdfFunction($text = null)
-    {
-        $result = $text." in the way";
-        return $result;
-    }
+    $dompdf = new Dompdf($options);
+    $dompdf->loadHtml($html);
+
+    // (Optional) Setup the paper size and orientation
+    $dompdf->setPaper('A4', 'portrait');
+
+    $dompdf->render();
+    $dompdf->stream();
+    return '';
+  }
 }
